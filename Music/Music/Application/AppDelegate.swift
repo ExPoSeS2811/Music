4101bd3645ca809fc5da85b096a1aba20ff5a088
Gleb Rasskazov <glebunium146@gmail.com>
2023-09-24 20:49:13 +0300

    Initial commit

---
+++ Music/Music/Application/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Music
+//
+//  Created by Gleb Rasskazov on 24.09.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        if let window = window {
+            window.rootViewController = LoginViewController()
+            window.makeKeyAndVisible()
+        }
+        return true
+    }
+}
---
+++ Music/Music/Modules/Login/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Music
+//
+//  Created by Gleb Rasskazov on 24.09.2023.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+
+}
+
