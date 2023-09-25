584585c00e60733262ba69f5bcc824619d104a96
fullzoom <fullzoom@MacBook-Pro-fullzoom.local>
2023-09-25 19:37:36 +0200

    Fix Bug

--- Music/Music/Application/AppDelegate.swift
+++ Music/Music/Application/AppDelegate.swift
@@ -7,8 +7,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
         if let window = window {
-            window.rootViewController = OnboardingViewController(image: UIImage(named: "Onboarding1")!, headerTitle: "Welcome to MusicApp: Your favorite tracks in one place", title: "Welcome to MusicApp, a unique music app that offers you a limitless world of music in the palm of your hand")
-
+            window.rootViewController = CreateAccountViewController()
             window.makeKeyAndVisible()
         }
         return true
